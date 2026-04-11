68b5e6e21fc1b2f3d8ecb630bd5ae0eb73bd98d4
xshyrz <shaineyroseselgas@gmail.com>
2026-04-11 15:49:53 +0800

    rename

--- employeeSalaryCal.java
+++ employeeSalaryCal.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class employeeSalaryCalcuator {
+public class employeeSalaryCal {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
